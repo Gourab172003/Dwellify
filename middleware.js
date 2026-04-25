159d88d99dd94f7d2a7510b72df464855c5476a8
Gourab <gssarkargss2003@gmail.com>
2026-04-25 12:21:52 +0530

    .

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLogedin=(req,res,next)=>{
     if(!req.isAuthenticated()) {
+      req.session.redirectUrl=req.originalUrl;
     req.flash("error", "You must be logged in to add new Property");
     return res.redirect("/login");
     next();

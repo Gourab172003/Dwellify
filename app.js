08f255aa8000741b3f0f019058561abe0c6e5637
Gourab Sarkar <gssarkargss2003@gmail.com>
2026-04-27 13:47:37 +0530

    Fix database name mismatch (wonderlust -> wanderlust)

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ main()
     .catch(err => console.log("DB Error:", err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
 // -----------------------------------------App Config-----------------------------------------
